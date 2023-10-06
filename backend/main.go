7618c17f2612be29ed4aed505632cd3315d87eab
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-10-06 18:43:51 -0400

    Refactored main.go

--- backend/main.go
+++ backend/main.go
@@ -7,21 +7,29 @@ import (
 	"backend/pkg/websocket"
 )
 
-func serveWebsocket(w http.ResponseWriter, r *http.Request) {
-	ws, err := websocket.Upgrade(w, r)
+func serveWebsocket(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Websocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
 	if err != nil {
 		fmt.Fprintf(w, "%+V\n", err)
 	}
-	go websocket.Writer(ws)
-	websocket.Reader(ws)
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
 }
 
 func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Simple Server")
-	})
+	pool := websocket.NewPool()
+	go pool.Start()
 
-	http.HandleFunc("/ws", serveWebsocket)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWebsocket(pool, w, r)
+	})
 }
 
 func main() {

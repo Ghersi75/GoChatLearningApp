a638bacfba0ca34a45290c214c7d383f348b53fd
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-10-06 18:12:30 -0400

    Added Header and ChatHiostory

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,39 @@
-import Image from 'next/image'
+"use client"
+import ChatHistory from "@/components/ChatHistory"
+import { connect, sendMsg } from "@/utils/api/websocketHandler"
+import { useEffect, useState } from "react"
 
 export default function Home() {
+  const [msgHistory, setMsgHistory] = useState<string[]>([])
+  const [msg, setMsg] = useState("")
+
+  useEffect(() => {
+    connect((msg: any) => {
+      setMsgHistory(prev => {
+        return [...prev, msg]
+      })
+    })
+  }, [])
+
+  const handleClick = () => {
+    if (msg == "") {
+      return
+    }
+    sendMsg(msg)
+    setMsg("")
+  }
+
   return (
-    <main className="">
-      Test
+    <main className="bg-slate-200">
+      <div className="flex flex-col w-screen h-fit justify-center items-center">
+        <div className="p-2.5 flex flex-col justify-center items-center gap-4">
+          <input type="text p-2.5 bg-black" value={msg} onChange={e => setMsg(e.target.value)}></input>
+          <button onClick={handleClick} className="bg-white w-fit h-fit rounded-lg p-2.5 text-xl">
+            Send
+          </button>
+        </div>
+        <ChatHistory chatHistory={msgHistory} />
+      </div>
     </main>
   )
 }

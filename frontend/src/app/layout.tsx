9f5ec1d84a9e1f7d3c8444faf8ce5751aa2fd993
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-10-06 16:32:41 -0400

    Added header to layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Header from "@/components/Header"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header className="bg-blue-900 text-white text-xl font-bold pl-2.5 py-4"/>
+        {children}
+      </body>
     </html>
   )
 }
